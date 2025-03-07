c7c1810bfc90e7d04e6a10e7150851b7182d307f
Jairo Bonilla C <jairobonillacalvo@gmail.com>
2025-03-07 15:59:38 -0600

    Deploy Vue.js in Cloud Run

---
+++ project/gcr-vue-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Welcome to Vue on Cloud Run! 🚀</h1>
+    <p>Deployed via GitHub Actions.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+h1 {
+  color: #42b983;
+  text-align: center;
+}
+</style>
